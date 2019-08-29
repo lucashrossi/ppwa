2c44954838da410fcd7f162c03b9c7c5bfca77d0
lucashrossi <lucashrossi@gmail.com>
2019-08-29 17:52:05 -0300

    pwa

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,115 @@
 <template>
-  <v-container fluid grid-list-md style="margin-top: 5%;">
-    <v-layout
-      row
-      wrap
-    >
-      <v-flex
-        align-left
-        xs2
-        sm8
-        md6
-        lg1
+  <div>
+    <v-container fluid grid-list-md style="margin-top: 5%;">
+      <v-layout
+        row
+        wrap
       >
-        <div>
-          <v-img
-            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-            contain
-            max-width="70%"
-          />
-        </div>
-      </v-flex>
-      <v-spacer />
-      <v-flex
-        align-left
-        xs2
-        sm8
-        md6
-        lg1
+        <v-flex
+          align-left
+          xs2
+          sm8
+          md6
+          lg1
+        >
+          <div>
+            <v-img
+              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
+              contain
+              max-width="70%"
+            />
+          </div>
+        </v-flex>
+        <v-spacer />
+        <v-flex
+          align-left
+          xs2
+          sm8
+          md6
+          lg1
+        >
+          <div>
+            <v-img
+              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
+              contain
+              max-width="70%"
+            />
+          </div>
+        </v-flex>
+        <v-flex
+          align-left
+          xs2
+          sm8
+          md6
+          lg1
+        >
+          <div>
+            <v-img
+              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
+              contain
+              max-width="70%"
+            />
+          </div>
+        </v-flex>
+        <v-spacer />
+      </v-layout>
+    </v-container>
+    <v-container fluid grid-list-md style="margin-top: 10%;">
+      <v-layout
+        row
+        wrap
       >
-        <div>
-          <v-img
-            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-            contain
-            max-width="70%"
-          />
-        </div>
-      </v-flex>
-      <v-flex
-        align-left
-        xs2
-        sm8
-        md6
-        lg1
-      >
-        <div>
-          <v-img
-            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-            contain
-            max-width="70%"
-          />
-        </div>
-      </v-flex>
-      <v-spacer />
-    </v-layout>
-  </v-container>
+        <v-flex
+          align-left
+          xs2
+          sm8
+          md6
+          lg1
+        >
+          <div>
+            <v-img
+              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
+              contain
+              max-width="70%"
+            />
+          </div>
+        </v-flex>
+        <v-spacer />
+        <v-flex
+          align-left
+          xs2
+          sm8
+          md6
+          lg1
+        >
+          <div>
+            <v-img
+              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
+              contain
+              max-width="70%"
+            />
+          </div>
+        </v-flex>
+        <v-spacer />
+        <v-flex
+          align-left
+          xs2
+          sm8
+          md6
+          lg1
+        >
+          <div>
+            <v-img
+              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
+              contain
+              max-width="70%"
+            />
+          </div>
+        </v-flex>
+        <v-spacer />
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
