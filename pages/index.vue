34b5580770178f952495ab3dd29b5101d260b562
lucashrossi <lucashrossi@gmail.com>
2019-09-03 15:54:02 -0300

    pwa

--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,136 @@
 <template>
-  <div>
-    <v-container fluid grid-list-md style="margin-top: 5%;">
-      <v-layout
-        row
-        wrap
-      >
-        <v-flex
-          align-left
-          xs2
-          sm8
-          md6
-          lg1
-        >
-          <div>
-            <v-img
-              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-              contain
-              max-width="70%"
+  <!-- <v-container>
+    <v-layout>
+      <v-flex>
+        <div>I'm a flexbox container!</div>
+      </v-flex>
+    </v-layout>
+  </v-container> -->
+  <v-container>
+    <v-layout style="margin-top: 5%; margin-bottom: -1.5%;">
+      <v-flex row class="hidden-md-and-down" lg2 xl2>
+        <div>
+          <v-img
+            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatCC.png?alt=media&token=a1d032b0-53eb-4008-b688-3022269b4497"
+            contain
+            max-width="50%"
+            style="margin-left: 10%"
+          />
+        </div>
+      </v-flex>
+      <v-flex row xs10 justify-center>
+        <div>
+          <v-img
+            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatCC.png?alt=media&token=a1d032b0-53eb-4008-b688-3022269b4497"
+            contain
+            max-width="50%"
+          />
+        </div>
+        <div>
+          <v-img
+            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatCC.png?alt=media&token=a1d032b0-53eb-4008-b688-3022269b4497"
+            contain
+            max-width="50%"
+          />
+        </div>
+        <div>
+          <v-img
+            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatCC.png?alt=media&token=a1d032b0-53eb-4008-b688-3022269b4497"
+            contain
+            max-width="50%"
+          />
+        </div>
+        <div>
+          <v-img
+            src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatCC.png?alt=media&token=a1d032b0-53eb-4008-b688-3022269b4497"
+            contain
+            max-width="50%"
+          />
+        </div>
+      </v-flex>
+    </v-layout>
+    <div
+      style="height: 40px; border-bottom: 2px solid green;  border-left: 2px solid green;"
+    />
+    <v-layout style="margin-top: 10%;" justify-center>
+      <v-flex row>
+        <v-flex>
+          <v-flex row justify-center>
+            <v-layout style="margin-top: 1%; margin-bottom: -3%;">
+              <v-flex row class="hidden-md-and-down" lg2 xl2>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
+                    contain
+                    max-width="90%"
+                    style="margin-left: 11%"
+                  />
+                </div>
+              </v-flex>
+              <v-flex row justify-center xs12>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex row justify-left>
+            <div
+              style="width: 95%; height: 40px; border-bottom: 2px solid green;  border-left: 2px solid green;"
             />
-          </div>
+          </v-flex>
         </v-flex>
-        <v-spacer />
-        <v-flex
-          align-left
-          xs2
-          sm8
-          md6
-          lg1
-        >
-          <div>
-            <v-img
-              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-              contain
-              max-width="70%"
+        <v-flex>
+          <v-flex row justify-center>
+            <v-layout style="margin-top: 1%; margin-bottom: -3%;">
+              <v-flex row class="hidden-md-and-down" lg2 xl2>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCCC.png?alt=media&token=8425f225-c036-43b0-855a-f4135d8d8716"
+                    contain
+                    max-width="90%"
+                    style="margin-left: 15%"
+                  />
+                </div>
+              </v-flex>
+              <v-flex row justify-center>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCCC.png?alt=media&token=8425f225-c036-43b0-855a-f4135d8d8716"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCCC.png?alt=media&token=8425f225-c036-43b0-855a-f4135d8d8716"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex row justify-left>
+            <div
+              style="width: 100%; height: 40px; border-bottom: 2px solid green;  border-left: 2px solid green;"
             />
-          </div>
+          </v-flex>
         </v-flex>
-        <v-flex
-          align-left
-          xs2
-          sm8
-          md6
-          lg1
-        >
-          <div>
-            <v-img
-              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-              contain
-              max-width="70%"
-            />
-          </div>
-        </v-flex>
-        <v-spacer />
-      </v-layout>
-    </v-container>
-    <v-container fluid grid-list-md style="margin-top: 10%;">
-      <v-layout
-        row
-        wrap
-      >
-        <v-flex
-          align-left
-          xs2
-          sm8
-          md6
-          lg1
-        >
-          <div>
-            <v-img
-              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-              contain
-              max-width="70%"
-            />
-          </div>
-        </v-flex>
-        <v-spacer />
-        <v-flex
-          align-left
-          xs2
-          sm8
-          md6
-          lg1
-        >
-          <div>
-            <v-img
-              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-              contain
-              max-width="70%"
-            />
-          </div>
-        </v-flex>
-        <v-spacer />
-        <v-flex
-          align-left
-          xs2
-          sm8
-          md6
-          lg1
-        >
-          <div>
-            <v-img
-              src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/WheatC.png?alt=media&token=5e90872f-1a80-4fb9-8258-70b5fdfaa10e"
-              contain
-              max-width="70%"
-            />
-          </div>
-        </v-flex>
-        <v-spacer />
-      </v-layout>
-    </v-container>
-  </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
