b2364ec85fa18d014e9e25f46de5f324735416cc
lucashrossi <lucashrossi@gmail.com>
2019-09-03 11:59:14 -0300

    pwa

--- pages/Empresa/index.vue
+++ pages/Empresa/index.vue
@@ -7,7 +7,7 @@
     </v-layout>
   </v-container> -->
   <v-container>
-    <v-layout style="margin-top: 5%; padding-bottom: 2%; border-bottom: 2px solid green;">
+    <v-layout style="margin-top: 5%; margin-bottom: -1.5%;">
       <v-flex row class="hidden-md-and-down" lg2 xl2>
         <div>
           <v-img
@@ -49,63 +49,85 @@
         </div>
       </v-flex>
     </v-layout>
+    <div
+      style="height: 40px; border-bottom: 2px solid green;  border-left: 2px solid green;"
+    />
     <v-layout style="margin-top: 10%;" justify-center>
-      <v-flex row xs12>
-        <v-layout style="margin-top: 1%">
-          <v-flex row class="hidden-md-and-down" lg2 xl2>
-            <div>
-              <v-img
-                src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
-                contain
-                max-width="90%"
-              />
-            </div>
-          </v-flex>
-          <v-flex row justify-center xs12>
-            <div>
-              <v-img
-                src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
-                contain
-                max-width="90%"
-              />
-            </div>
-            <div>
-              <v-img
-                src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
-                contain
-                max-width="90%"
-              />
-            </div>
-          </v-flex>
-        </v-layout>
-        <v-layout style="margin-top: 1%">
-          <v-flex row class="hidden-md-and-down" lg2 xl2>
-            <div>
-              <v-img
-                src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCC.png?alt=media&token=7538a711-3c18-45c5-b088-62a0a263da02"
-                contain
-                max-width="90%"
-                style="margin-left: 10%"
-              />
-            </div>
-          </v-flex>
+      <v-flex row>
+        <v-flex>
           <v-flex row justify-center>
-            <div>
-              <v-img
-                src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCC.png?alt=media&token=7538a711-3c18-45c5-b088-62a0a263da02"
-                contain
-                max-width="90%"
-              />
-            </div>
-            <div>
-              <v-img
-                src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCC.png?alt=media&token=7538a711-3c18-45c5-b088-62a0a263da02"
-                contain
-                max-width="90%"
-              />
-            </div>
+            <v-layout style="margin-top: 1%; margin-bottom: -3%;">
+              <v-flex row class="hidden-md-and-down" lg2 xl2>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
+                    contain
+                    max-width="90%"
+                    style="margin-left: 11%"
+                  />
+                </div>
+              </v-flex>
+              <v-flex row justify-center xs12>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/GirasolC.png?alt=media&token=94d8832b-5b1a-4a39-9652-34a727f6cb00"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+              </v-flex>
+            </v-layout>
           </v-flex>
-        </v-layout>
+          <v-flex row justify-left>
+            <div
+              style="width: 95%; height: 40px; border-bottom: 2px solid green;  border-left: 2px solid green;"
+            />
+          </v-flex>
+        </v-flex>
+        <v-flex>
+          <v-flex row justify-center>
+            <v-layout style="margin-top: 1%; margin-bottom: -4%;">
+              <v-flex row class="hidden-md-and-down" lg2 xl2>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCCC.png?alt=media&token=8425f225-c036-43b0-855a-f4135d8d8716"
+                    contain
+                    max-width="90%"
+                    style="margin-left: 15%"
+                  />
+                </div>
+              </v-flex>
+              <v-flex row justify-center>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCCC.png?alt=media&token=8425f225-c036-43b0-855a-f4135d8d8716"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+                <div>
+                  <v-img
+                    src="https://firebasestorage.googleapis.com/v0/b/pfdbb-dc48b.appspot.com/o/AvenaCCC.png?alt=media&token=8425f225-c036-43b0-855a-f4135d8d8716"
+                    contain
+                    max-width="90%"
+                  />
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex row justify-left>
+            <div
+              style="width: 100%; height: 40px; border-bottom: 2px solid green;  border-left: 2px solid green;"
+            />
+          </v-flex>
+        </v-flex>
       </v-flex>
     </v-layout>
   </v-container>
